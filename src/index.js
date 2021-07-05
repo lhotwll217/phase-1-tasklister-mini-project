6553c1e74a32240f0f2dcabc2feb8fc67e0cd9f3
Luke Otwell <lukehenryotwell@gmail.com>
2021-07-05 16:52:56 -0400

    My Site

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+
+  newTaskForm = document.getElementById("create-task-form"),
+
+    newTaskForm.addEventListener("submit", e => {
+
+      e.preventDefault()
+
+      let newTask = document.createElement('li')
+      let btn = document.createElement('button')
+
+      btn.innerText = "x"
+      newTask.textContent = e.target[0].value
+
+      newTask.append(btn)
+
+
+
+      console.log(newTask)
+
+
+
+      document.querySelector("#tasks").prepend(newTask)
+      e.target.reset()
+      // e.reset()
+
+
+
+
+      // newTask.append
+
+
+
+
+    })
+
+})
+
+
